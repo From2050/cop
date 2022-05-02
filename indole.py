cf780c9916d4e82665347558af0fa845097aecd6
Jay <m46012002@gmail.com>
2022-05-02 19:07:50 +0800

    ok

---
+++ indole.py
@@ -0,0 +1,13 @@
+# import tkinter as tk
+# from tkinter import filedialog
+# import mne
+# import matplotlib.pyplot as plt
+
+
+def main():
+    print('hellow')
+
+
+
+if __name__ == '__main__':
+    main()
