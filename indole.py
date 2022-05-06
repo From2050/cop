3075c737930ccda63bfb19fa7b27da2adb12c7cd
Jay <m46012002@gmail.com>
2022-05-07 04:43:31 +0800

    test

--- indole.py
+++ indole.py
@@ -1,14 +1,33 @@
-# import tkinter as tk
-# from tkinter import filedialog
-# import mne
-# import matplotlib.pyplot as plt
+import tkinter as tk
+from tkinter import filedialog
+import numpy as np
+import mne
+import matplotlib.pyplot as plt
 
 
 def main():
     print('hellow')
+    print('this is cop data plot')
 
-    print('this is test for vs stutio')
-    ##aaaaaa
+    filepath = "C:/Users/Jay/Desktop/2022_04_27_16_48_41/2022_04_27_16_48_41_insoleL.txt"
+    #with open(filepath,'r') as f:
+    #    for line in lines
+    Ain = np.loadtxt(filepath,dtype=str,delimiter=',')
+
+    Atime, Adata = np.hsplit(Ain, [1])
+    Adata = Adata.reshape(-1)                   #expand to line
+    Atime = Atime[np.where(Adata == "CA")]      # getting time tag by finding index of CA  
+    
+    
+
+
+
+
+    ##aa
+
+
+
+
 
 
 
