22436e98a719f4b886857e1d3eade1aab2f2b265
From2050 <m46012002@gmail.com>
2022-05-22 14:49:05 +0800

    vs

--- indole.py
+++ indole.py
@@ -12,37 +12,42 @@ def main():
     filepath = "./2022_04_27_16_48_41/2022_04_27_16_48_41_insoleL.txt"
 
     with open(filepath,'r') as f:
-        #Atemp = np.zeros(227+5,dtype=bytes)
-        #Aframe = np.zeros(227+5,dtype=bytes)
-        Atemp = np.zeros(227+5,dtype=int)
-        Aframe = np.zeros(227+5,dtype=int)
+        
+        Atemp = np.zeros(227+4,dtype=int)   # time(4)  + CA(1) + framenumber(1)  +data(8*28 = 224) + 0A (1) 
+        Aframe = np.zeros(227+4,dtype=int)
         index = 0
+        error = 0
         for line in f :
-            print(l)
+            print(line)
             temp = line.rstrip().split(',')
-            if temp[1] == 'CA':
-                Aframe = np.vstack([Aframe, Atemp])
-                time = temp[0].split('.')
-                #Atemp[0] = bytes([int(time[0][0:2])])
-                #Atemp[1] = bytes([int(time[0][3:5])])
-                #Atemp[2] = bytes([int(time[0][6:8])])
-                #Atemp[3] = bytes([int(time[1][0:2])])
-                Atemp[0] = int(time[0][0:2])
-                Atemp[1] = int(time[0][3:5])
-                Atemp[2] = int(time[0][6:8])
-                Atemp[3] = int(time[1][0:2])
-                index = 0
-            else :
-                for i in range(0,int(len(temp[1])),2):
-                    #if i == 0 : print(temp[1])
-                    #Atemp[(4+i)] = bytes.fromhex(temp[1][2*i:2*i+2])
+            for i in range(0,int(len(temp[1])),2):
+                if temp[1][i:i+2] == 'CA':
+                    Aframe = np.vstack([Aframe, Atemp])
+                    time = temp[0].split('.')
+                    Atemp[0] = int(time[0][0:2])
+                    Atemp[1] = int(time[0][3:5])
+                    Atemp[2] = int(time[0][6:8])
+                    Atemp[3] = int(time[1][0:2])
+                    index = 0
+                    
+                else :
+                    if index == 227 :
+                        index = 0
+                        error = error + 1 
                     Atemp[(4+index)] = int(temp[1][i:i+2],16)
                     index = index + 1
-                    #if i == 0 : print(Atemp[(4+i)])
+                
+                #    for i in range(0,int(len(temp[1])),2):
+                #        #if i == 0 : print(temp[1])
+                #        #Atemp[(4+i)] = bytes.fromhex(temp[1][2*i:2*i+2])
+                #        Atemp[(4+index)] = int(temp[1][i:i+2],16)
+                #        index = index + 1
+                #        #if i == 0 : print(Atemp[(4+i)])
 
-
-
-
+    Atest = Aframe[2:,6:-1]
+    Atest = Atest.reshape((413,28,8))
+    sns.heatmap(data=Atest[0],square=True)
+    plt.show()
 
     #Ain = np.loadtxt(filepath,dtype=str,delimiter=',')
 
